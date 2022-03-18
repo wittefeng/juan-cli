ae72a550319a44df34d77a1ac46f2c263e723381
wittefeng <wittefeng@gmail.com>
2022-03-18 23:48:51 +0800

    feat: add cli-spinners to roa

--- utils/utils/lib/spinner.js
+++ utils/utils/lib/spinner.js
@@ -1,9 +1,14 @@
 'use strict'
 
 const ora = require('ora')
+const cliSpinners = require('cli-spinners')
 
-function spinnerStart(msg = '正在加载中...') {
-  const spinner = ora(msg).start()
+function spinnerStart(msg = '正在加载中...', name = 'aesthetic') {
+  console.log('spinners name', name)
+  const spinner = ora({
+    text: msg,
+    spinner: cliSpinners[name]
+  }).start()
   return spinner
 }
 
